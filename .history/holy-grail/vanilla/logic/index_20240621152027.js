cae65fda1aba055076c21cf9b45bf4d83010de93
isafck <insaneesz1e@gmail.com>
2024-06-21 15:25:17 +0530

    holyGrail

---
+++ .history/holy-grail/vanilla/logic/index_20240621152027.js
@@ -0,0 +1,12 @@
+// Example of adding interactivity (not necessary for layout)
+document.getElementById('left').addEventListener('click', function() {
+    alert('You clicked the left column!');
+});
+
+document.getElementById('center').addEventListener('click', function() {
+    alert('You clicked the center column!');
+});
+
+document.getElementById('right').addEventListener('click', function() {
+    alert('You clicked the right column!');
+});
