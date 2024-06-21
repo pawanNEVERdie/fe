13adc01b5ace6ef986f9d1598bb7ab9c86c986c4
isafck <insaneesz1e@gmail.com>
2024-06-21 15:21:50 +0530

    holyGrail

---
+++ holy-grail/vanilla/logic/index.js
@@ -0,0 +1,12 @@
+
+document.getElementById('left').addEventListener('click', function() {
+    alert('You clicked the left column!');
+});
+
+document.getElementById('center').addEventListener('click', function() {
+    alert('You clicked the center column!');
+});
+
+document.getElementById('right').addEventListener('click', function() {
+    alert('You clicked the right column!');
+});
